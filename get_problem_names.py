d35cf240d8686c024ed0399a7019eae8d5c73bb2
Piotr Kończyk <pkonczyk1@gmail.com>
2024-03-22 09:30:31 +0100

    Begin implementing parsing method by beautiful soup

--- get_problem_names.py
+++ get_problem_names.py
@@ -1,12 +1,6 @@
 import sys
 import requests
-from html.parser import HTMLParser
-
-class SimpleHTMLParser(HTMLParser):
-    def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]) -> None:
-        print("starttag", tag)
-    def handle_data(self, data):
-        print("data", data)
+from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
 
@@ -16,16 +10,22 @@ if __name__ == "__main__":
     args = sys.argv
     match len(args):
         case 2:
-            url = base_url + args[1]
+            if args[1] in groups:
+                url = base_url + args[1]
+            else:
+                print("Group isn't recognized! Available groups are:\n" + "/".join(groups) + "\n")
+                sys.exit(1)
         case _:
-            print("Specify problem group. Available groups are:\n" + "/".join(groups) + "\n")
+            print("Specify problem group! Available groups are:\n" + "/".join(groups) + "\n")
             sys.exit(1)
 
     response = requests.get(url)
+    with open("response.html", "w") as f:
+        f.write(response.text)
     
     if response.status_code == 200:
-        parser = SimpleHTMLParser()
-        parser.feed(response.text)
-
+        soup = BeautifulSoup(response.text, features="html.parser")
+        table = soup.find("table")
+        print(len(table.find_all("a"))) #TODO: parse links to get problem id's
 
     
